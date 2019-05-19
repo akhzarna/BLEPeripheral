40cf72ecfe68b70a340b18c798e29b19ba0b6383
tahirhameed99 <tahir.hameed99@gmail.com>
2019-05-19 14:05:01 +0500

    Initial Commit

---
+++ NewApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NewApp
+//
+//  Created by tahir hameed on 19/05/2019.
+//  Copyright © 2019 letlechnologies. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NewApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by tahir hameed on 19/05/2019.
+//  Copyright © 2019 letlechnologies. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
